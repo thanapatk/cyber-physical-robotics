cd06154f9b25a6f76739ac3529c51d69c1956d69
Thanapat Kemthong <thanapat.kem@gmail.com>
2025-10-10 11:33:53 +0700

    feat: update main code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
-import time
 
-import numpy as np
+from tqdm import tqdm
 
 from core.board import Board
 from core.enums import Direction, TeamEnum
@@ -10,10 +9,10 @@ from core.simulation import SimulationController
 from robot import Robot
 from utils.visualization import Visualizer
 
-TARGET_FPS = 100
+TOTAL_GOLD_COUNT = 40
 
 if __name__ == "__main__":
-    board = Board(deposit_pos=((9, 0), (9, 19)), total_gold_count=40)
+    board = Board(deposit_pos=((9, 0), (9, 19)), total_gold_count=TOTAL_GOLD_COUNT)
 
     red_team_info = TeamInfo(team=TeamEnum.RED, deposit_pos=board.red_deposit_pos)
     blue_team_info = TeamInfo(team=TeamEnum.BLUE, deposit_pos=board.blue_deposit_pos)
@@ -30,32 +29,20 @@ if __name__ == "__main__":
     ]
 
     simulation_controller = SimulationController(robots=robots, board=board)
-    step_times = []
 
-    robots_set = set(robots)
+    for i in tqdm(range(10_000)):
+        if (
+            board.get_deposit_tile(team=TeamEnum.RED).gold_count
+            + board.get_deposit_tile(team=TeamEnum.BLUE).gold_count
+            == TOTAL_GOLD_COUNT
+        ):
+            break
 
-    for i in range(1_000):
-        start_loop_time = time.perf_counter()
         step = simulation_controller.step_count
         state = simulation_controller.get_simulation_state()
 
         Visualizer.visualize_board(step, state)
-
-        # for robot in robots:
-        #     print(f"{robot.robot_id}: {len(robot.sensed_map)}")
-        # print(
-        #     np.int64(
-        #         robots[i % len(robots)].get_coldness_map(i) * 1.5
-        #         + robots[i % len(robots)].generate_cost_matrix()
-        #     )
-        # )
-
-        start = time.perf_counter_ns()
         simulation_controller.step()
-        step_times.append(time.perf_counter_ns() - start)
 
-        time.sleep(max(0, 1 / TARGET_FPS - (time.perf_counter() - start_loop_time)))
-
-    print(f"Mean step time: {np.mean(step_times) * 1e-9/1e-3} ms")
-    print(f"Median step time: {np.median(step_times) * 1e-9/1e-3} ms")
-    print(f"Std step time: {np.std(step_times) * 1e-9/1e-3} ms")
+    print(f"RED SCORE: {board.get_deposit_tile(team=TeamEnum.RED).gold_count}")
+    print(f"BLUE SCORE: {board.get_deposit_tile(team=TeamEnum.BLUE).gold_count}")
